0c4ad352f9d2be2281f6b6324621b64b897f49eb
privatetan <privatetantao@gmail.com>
2022-08-22 09:23:38 +0800

    搭建项目

---
+++ src/main/java/org/study/microservicescaffold/MicroServiceScaffoldApplication.java
@@ -0,0 +1,13 @@
+package org.study.microservicescaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/study/microservicescaffold/MicroServiceScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.microservicescaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
